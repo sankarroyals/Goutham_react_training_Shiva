9aab5632d8c6884c0aa232ecfbfa9c0973be9212
ShivaShankar <ss3177806@gmail.com>
2023-12-08 10:46:24 +0530

    starter file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Parent from './Components/Parent/Parent';
+
 
 function App() {
   return (
     <div className="App">
-      <Parent />
+    
     </div>
   );
 }
