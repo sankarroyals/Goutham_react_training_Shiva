b98f34ee11cbf0ef876b03d14295028af86fdeb1
sankar mukesh <ysankarmukesh@gmail.com>
2023-12-12 17:02:41 +0530

    Useparams

--- src/Components/Items/Item.js
+++ src/Components/Items/Item.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 
-const Item = ({d}) => {
-    const navigate= useNavigate()
+const Item = ({ d }) => {
+  const navigate = useNavigate()
   return (
-    <div style={{padding: "10px", border: "2px solid black"}}>
-    <div>{d.product}</div>
-    <div>{d.price}</div>
-    <button onClick={() => navigate(`/items/${d.id}/${d.product}`)}>
+    <div style={{ padding: "10px", border: "2px solid black" }}>
+      <div>{d.product}</div>
+      <div>{d.price}</div>
+      <button onClick={() => navigate(`/items/${d.id}/${d.product}`)}>
         go to the product
-    </button>
+      </button>
     </div>
   )
 }
--- src/Components/Items/Singleproduct.js
+++ src/Components/Items/Singleproduct.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
 
 const Singleproduct = () => {
-    const {id,product}=useParams()
+  const { id, product } = useParams()
+  const navigate =  useNavigate()
+  // const [singleData, setSingleData] = useState({})
+  // useEffect(() => {
+    // axios.get(`http://products/${id}`).then(res => {
+    //   setSingleData(res.data)
+    // })
+  // }, [id])
+  
   return (
-    <div style={{padding: "10px", border: "2px solid black", width: "150px"}}>
-    <div>{id}</div>
-    <div>{product}</div>
-    </div>
+    <>
+      <div onClick={()=>navigate(-1)} style={{cursor: 'pointer'}}>Back</div>
+      <div style={{ padding: "10px", border: "2px solid black", width: "150px", marginTop: '10px' }}>
+        <div>{id}</div>
+        <div>{product}</div>
+      </div>
+    </>
+    
   )
 }
 
