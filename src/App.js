187b409d9b44f18acbea00426042ee366cbe23a6
ShivaShankar <ss3177806@gmail.com>
2023-12-12 13:14:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     <div>
         <center>
         <h1>useState Example</h1>
-       <h1>      {name}</h1>
+       <h1>{name}</h1>
             <button onClick={() => setName("Shiva Shankar")}>Change</button>
         </center>
     </div>
