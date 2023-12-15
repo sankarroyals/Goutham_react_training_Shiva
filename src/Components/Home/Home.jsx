ccc6dc895db6750b1ade857b33373841dbc1ec61
ShivaShankar <ss3177806@gmail.com>
2023-12-15 15:22:07 +0530

    added some changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-     <>
-       <div className='home_Content'>
-          Welcome to Shopping Cart
-    </div>
-    <p>Please go to Products</p>
-     </>
-  )
-}
+    <>
+      <div className='home_Content'>
+        <h1>Welcome to Shopping Cart</h1>
+        <p>Explore our products and start building your shopping cart!</p>
+      </div>
+      <div className='shopping_links'>
+        <p>Please go to Products to view our available items.</p>
+        <p>Already have items in your cart? <a href='/cart'>View Cart</a></p>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
