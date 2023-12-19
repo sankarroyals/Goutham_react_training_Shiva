5ee0a7f8ee5118c45699aa08cd29574cb1a9a42d
ShivaShankar <ss3177806@gmail.com>
2023-12-19 11:46:36 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Hoc from './Hoc';
 
-const App =({name}) => {
+const buttonStyle = {
+  padding: '10px',
+  margin: '5px',
+  backgroundColor: '#4CAF50', 
+  color: 'white',
+  border: 'none',
+  borderRadius: '3px',
+  cursor: 'pointer',
+};
+
+const App = ({ name, onLogout }) => {
   return (
-    <h1>Welcome Mr. {name}</h1>
-  )
-}
+    <div>
+      <h1>Welcome Mr. {name}</h1>
+      <button onClick={onLogout} style={buttonStyle}>
+        Logout
+      </button>
+    </div>
+  );
+};
+
+
 
 export default Hoc(App);
--- src/Hoc.js
+++ src/Hoc.js
@@ -1,20 +1,43 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+
+const buttonStyle = {
+  padding: '10px',
+  margin: '5px',
+  backgroundColor: '#008CBA', 
+  color: 'white',
+  border: 'none',
+  borderRadius: '3px',
+  cursor: 'pointer',
+};
+
 
 const Hoc = (WrappedComponent) => {
   const WithAuth = (props) => {
     const [auth, setAuth] = useState(false);
 
+    const handleLogin = () => {
+      setAuth(true);
+    };
+
+    const handleLogout = () => {
+      setAuth(false);
+    };
+
     return (
       <div>
         {auth ? (
-          <WrappedComponent name={"Shiva Shankar Goddumarri"} />
+          <WrappedComponent name={'Shiva Shankar Goddumarri'} onLogout={handleLogout} />
         ) : (
-          <h1>Please Login....</h1>
+          <div>
+            <h1>Please Login....</h1>
+            <button onClick={handleLogin} style={buttonStyle}>
+              Login
+            </button>
+          </div>
         )}
       </div>
     );
   };
-
   return WithAuth;
 };
 
