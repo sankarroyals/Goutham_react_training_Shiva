331b77a0fb6b14a033f632c7a011422759d3fc4d
ShivaShankar <ss3177806@gmail.com>
2023-12-20 13:34:54 +0530

    styles updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ComponentC from "./Component_C";
+import "./App.css"; 
 export const UserContext = React.createContext();
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <center>
         <UserContext.Provider value={"Shiva"}>
           <ComponentC />
