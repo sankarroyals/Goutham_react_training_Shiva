ba7cb19ea9cd48daf746272173d43c882c330b92
ShivaShankar <ss3177806@gmail.com>
2023-12-20 13:07:08 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react'
+import { useEffect } from 'react';
 
+const App = () => {
+  const data = useRef(null);
+  const submitHandler = e =>{
+    e.preventDefault();
+    console.log(data.current.value);
 
-function App() {
+    // updating
+    // data.current.value='shiva shankar';
+  }
+  useEffect(()=>{
+    data.current.focus();
+  },[]);
+
   return (
-    <div className="App">
-    
+    <div>
+      <center>
+        <form onSubmit={submitHandler}>
+          <input ref={data} type='text' placeholder='Enter your name' />
+          <input type='submit' />
+        </form>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
