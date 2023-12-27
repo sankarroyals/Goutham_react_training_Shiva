daf45c1356b8f9ce57e4f2fd0cdd360112701e6c
ShivaShankar <ss3177806@gmail.com>
2023-12-27 11:08:23 +0530

    icons added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import './Navbar.css'
-import { Link, useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import './Navbar.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faShoppingCart, faSignOutAlt, faBox } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-    const navigate = useNavigate()
-    const count = useSelector((state) => state.cartS.cart)  //get redux variables
+    const navigate = useNavigate();
+    const count = useSelector((state) => state.cartS.cart); // get redux variables
+
     return (
         <div className='navbar'>
-            <Link to='/'><div>Home</div></Link>
-            <Link to='/products'><div>Products</div></Link>
+            <Link to='/'>
+                <div>
+                    <FontAwesomeIcon icon={faHome} /> Home
+                </div>
+            </Link>
+            <Link to='/products'>
+                <div>
+                    <FontAwesomeIcon icon={faBox} /> Products
+                </div>
+            </Link>
             <Link to='/cart'>
-                <div style={{position: 'relative'}}>Cart
+                <div style={{ position: 'relative' }}>
+                    <FontAwesomeIcon icon={faShoppingCart} /> Cart
                     <div style={{ position: 'absolute', top: '-10px', right: '-14px', background: 'red', borderRadius: '50%', padding: '0px 6px' }}>
                         {count}
                     </div>
-                </div></Link>
+                </div>
+            </Link>
             <Link onClick={() => {
-                localStorage.removeItem('user')
-                window.location.href = '/'
-            }}><div>Logout</div></Link>
-
-
+                localStorage.removeItem('user');
+                window.location.href = '/';
+            }}>
+                <div>
+                    <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+                </div>
+            </Link>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
