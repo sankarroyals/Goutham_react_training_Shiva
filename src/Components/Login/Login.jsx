ffa808a0c5fe2a0212dacb599c669f39396c896b
ShivaShankar <ss3177806@gmail.com>
2023-12-29 10:37:36 +0530

    login image added , nav bar styles changed

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,50 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {jwtDecode} from 'jwt-decode'
-import './login.css'
+import { jwtDecode } from "jwt-decode";
+import "./login.css";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleCallback(response) {
-        console.log("Encoded", jwtDecode(response.credential));
-        localStorage.setItem('user', response.credential);
-        window.location.href = '/'
-    }
+  function handleCallback(response) {
+    console.log("Encoded", jwtDecode(response.credential));
+    localStorage.setItem("user", response.credential);
+    window.location.href = "/";
+  }
 
-    useEffect(() => {
-        window.google?.accounts.id.initialize({
-            client_id:
-                "883912602233-q4h2fm2a0lk9s8v2c6optve7c0h26bgh.apps.googleusercontent.com",
-            callback: handleCallback,
-        });
+  useEffect(() => {
+    window.google?.accounts.id.initialize({
+      client_id:
+        "883912602233-q4h2fm2a0lk9s8v2c6optve7c0h26bgh.apps.googleusercontent.com",
+      callback: handleCallback,
+    });
 
-        window.google?.accounts.id.renderButton(document.getElementById("signIn"), {
-            theme: "outline",
-            size: "large",
-        });
-    }, []);
+    window.google?.accounts.id.renderButton(document.getElementById("signIn"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
 
-    return (
-        <div>
-            <main class="main">
-                <div class="container">
-                    <section class="wrapper">
-                        <div
-                            style={{
-                                textAlign: "center",
-                                marginBottom: "20px",
-                                fontSize: "28px",
-                            }}
-                        >
-                            Google Sign In
-                        </div>
-                        <div id="signIn" style={{ marginLeft: "90px" }}></div>
-                    </section>
-                </div>
-            </main>
+  return (
+    <div>
+      <main class="main">
+        <div class="container">
+          <img
+            style={{
+              height: "100vh",
+              width: "50%",
+              marginLeft: "-180px",
+            }}
+            src="clothes.png"
+            alt="Clothes"
+          />
+          <section class="wrapper">
+            <div
+              style={{
+                textAlign: "center",
+                marginBottom: "20px",
+                fontSize: "28px",
+                marginTop: "80px",
+                marginLeft: "80px",
+                fontWeight: "600",
+              }}
+            >
+              Google Sign In
+            </div>
+            <p style={{ marginLeft: "80px" }}>
+              Welcome, Please login for shopping
+            </p>
+            <div id="signIn" style={{ marginLeft: "100px" }}></div>
+          </section>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,66 +1,106 @@
-import React from 'react';
-import './Navbar.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faShoppingCart, faSignOutAlt, faBox } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faShoppingCart,
+  faSignOutAlt,
+  faBox,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const count = useSelector((state) => state.cartS.cart); // get redux variables
-    const {email, picture, name} = useSelector((state)=> state.Auth.userDetails)
+  const navigate = useNavigate();
+  const count = useSelector((state) => state.cartS.cart); // get redux variables
+  const { email, picture, name } = useSelector(
+    (state) => state.Auth.userDetails
+  );
 
-    return (
-        <div className='navbar'>
-            <Link to='/'>
-                <div>
-                    <FontAwesomeIcon icon={faHome} /> Home
-                </div>
-            </Link>
-            <Link to='/products'>
-                <div>
-                    <FontAwesomeIcon icon={faBox} /> Products
-                </div>
-            </Link>
-            <Link to='/cart'>
-                <div style={{position: 'relative', display: 'flex', alignItems: 'center', gap: '2px' }}>
-                    <FontAwesomeIcon icon={faShoppingCart} style={{fontSize: '30px'}}/> <div>
-                    Cart
-                    </div>
-                    <div style={{position: 'absolute', top: '0px', left: '10px',  color: 'black', borderRadius: '50%', padding: '0px 6px', fontSize: '12px' , fontWeight: '800'}}>
-                        {count}
-                    </div>
-                </div>
-            </Link>
-
-            <div onClick={(e)=>{
-                document.getElementsByClassName('userDetails')[0].classList.toggle('showUserDetails')
-            }}>
-                <img src={picture} height={40} width={40}  style={{borderRadius: '50%', cursor: 'pointer'}}/>
-            </div>
-            <div className='userDetails'>
-            <div>
-                <img src={picture} height={100} width={100}  style={{borderRadius: '50%', cursor: 'pointer'}}/>
-            </div>
-            <div>
-                {name}
-            </div>
-            <div>
-            <div>
-            <Link onClick={() => {
-                localStorage.removeItem('user');
-                window.location.href = '/';
-            }}>
-                <div>
-                    <FontAwesomeIcon icon={faSignOutAlt} /> Logout
-                </div>
-            </Link> 
-                </div>
-            </div>
-            </div>
-            
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <div>
+          <FontAwesomeIcon icon={faHome} /> Home
         </div>
-    );
+      </Link>
+      <Link to="/products">
+        <div>
+          <FontAwesomeIcon icon={faBox} /> Products
+        </div>
+      </Link>
+      <Link to="/cart">
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            alignItems: "center",
+            gap: "2px",
+          }}
+        >
+          <FontAwesomeIcon icon={faShoppingCart} style={{ fontSize: "30px" }} />{" "}
+          <div>Cart</div>
+          <div
+            style={{
+              position: "absolute",
+              top: "0px",
+              left: "10px",
+              color: "black",
+              borderRadius: "50%",
+              padding: "0px 6px",
+              fontSize: "12px",
+              fontWeight: "800",
+            }}
+          >
+            {count}
+          </div>
+        </div>
+      </Link>
+
+      <div
+        onClick={(e) => {
+          document
+            .getElementsByClassName("userDetails")[0]
+            .classList.toggle("showUserDetails");
+        }}
+      >
+        <img
+          src={picture}
+          height={40}
+          width={40}
+          style={{ borderRadius: "50%", cursor: "pointer" }}
+        />
+      </div>
+      <div className="userDetails">
+        <div>
+        <div style={{fontWeight: '600', fontSize: '15px'}}>{email}</div>
+          <img
+            src={picture}
+            height={100}
+            width={100}
+            style={{ borderRadius: "50%", cursor: "pointer" }}
+          />
+        </div>
+        <div style={{fontWeight: '600', fontSize: '20px'}}>Hi, {name}!</div>
+        <div>
+          <div>
+            <Link
+              to="/"
+              className="logout-link"
+              onClick={() => {
+                localStorage.removeItem("user");
+                window.location.href = "/";
+              }}
+            >
+              <div className="logout">
+                <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
